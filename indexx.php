7335da45d4d15b3da289d7649abe72f523619c0f
Laurymassiel <166959514+Laurymassiel@users.noreply.github.com>
2024-04-16 14:52:51 -0700

    initial commit

---
+++ indexx.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Laser graf</title>
+    <link rel="stylesheet" href="Css/graf.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script defer src="../Lasergraf/Js/funsions.js"></script>
+</head>
+<body>
+    
+<header>
+    <div class="logo"></div>
+    <div class="inner-width">
+      <img src="https://i.ibb.co/m0K6W8d/zyro-image-18-Photoroom-png-Photoroom.png" alt="HOLA" width="260px" height="90">
+
+      <i class="menu-toggle-btn fas fa-bars"></i>
+      <nav class="navigation-menu">
+        <a href="http://localhost/lasergraf/indexx.php" class="aj_btn"><i class="fas fa-home home" ></i> Inicio</a>
+        <a href="#nosotros" class="aj_btn"></i> Nosotros</a>
+        <a href="http://localhost/hola/placas.php" class="aj_btn"></i> Productos</a>
+        <a href="http://localhost/lasergraf/servisios.php" class="aj_btn"></i> Servicios</a>
+        <a href="http://localhost/lasergraf/Formulario.php" class="aj_btn">Registrate</a>
+   </header>
+   <section id="container-slider">	
+   <a href="javascript: fntExecuteSlide('prev');" class="arrowPrev"><i class="fas fa-chevron-circle-left"></i></a>
+   <a href="javascript: fntExecuteSlide('next');" class="arrowNext"><i class="fas fa-chevron-circle-right"></i></a>
+   <ul class="listslider">
+     <li><a itlist="itList_0" href="#" class="item-select-slid"></a></li>
+     <li><a itlist="itList_1" href="#"></a></li>
+     <li><a itlist="itList_2" href="#"></a></li>
+   </ul>
+   <ul id="slider">
+     <li style="background-image: url(https://i.ibb.co/zF9G9ks/portada-3.png); z-index:0; opacity: 1;">
+       <div class="content_slider" >
+         <div>
+           <h2>Bienvenido</h2>
+	   <p>Descubra las diversas novedades que nuestro negocio ofrece.</p>
+	
+	 </div>
+       </div>
+     </li>
+     <li style="background-image: url(https://i.ibb.co/GWHtLhH/slider3.png); ">
+       <div class="content_slider" >
+         <div>
+           <h2>Diseños de calidad</h2>
+	   <p>Recursos, guías, mejores precios y comodidad para hacer realidad tu diseño.</p>
+	   
+	 </div>
+       </div>
+     </li>
+     <li style="background-image: url('https://i.ibb.co/YPLVGK2/ultima.png'); ">
+       <div class="content_slider" >
+         <div>
+           <h2>Innovación</h2>
+	   <p>Creatividad del diseño gráfico para ofrecer una experiencia visual única en tu hogar</p>
+
+	 </div>
+       </div>
+     </li>
+  </ul>
+</section>
+    <br>
+    <br>
+        <section class="nosotros" id="nosotros">
+        <h1 class="titulo_nosotros"> <span>Sobre</span> Nosotros</h1>
+
+        <div class="mp4_nosotros">
+            <div class="vid_nosotros">
+                <video src="img/harta.mp4" loop autoplay muted></video>
+              
+            </div>
+
+            <div class="txt_nosotros">
+                <h3>¿Quiénes somos?</h3>
+                <p>Laser Graf surge en el contexto de la era moderna de la fabricación digital, donde las tecnologías de corte y grabado láser se convierten en herramientas esenciales para la creación de una amplia gama de productos personalizados. Fundada por Pablo Roríguez en el 2010, Laser Graf inicialmente se centró en la fabricación de placas de acrílico para uso comercial, señalización y decoración.</p>
+            </div>
+        </div>
+
+</section>
+
+        
+<ul class="cards">
+  <li>
+    <a href="" class="card">
+      <img src="https://i.ibb.co/ZBb2zQb/mision.png" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <div class="card__header-text">
+            <h3 class="card__title">Misión</h3>            
+          </div>
+        </div>
+        <p class="card__description">En Laser Graf, nos comprometemos a ofrecer soluciones de diseño gráfico y de alta calidad que superen las expectativas de nuestros clientes. Nos comprometemos por ser líderes en creatividad, trabajando en estrecha colaboración con nuestros clientes para comprender sus necesidades y proporcionarles diseños que sean impresionantes ante su óptica.</p>
+      </div>
+    </a>      
+  </li>
+  <li>
+    <a href="" class="card">
+      <img src="https://i.ibb.co/sQMHt3H/vision.png" class="card__image" alt="" />
+      <div class="card__overlay">        
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
+          <div class="card__header-text">
+            <h3 class="card__title">Visión</h3>
+          </div>
+        </div>
+        <p class="card__description">Nos esforzamos por ser una empresa  que constantemente desafía los límites creativos y técnicos, ofreciendo soluciones únicas y revolucionarias que marquen tendencia en la industria del diseño. Asimismo, buscamos expandir nuestra presencia global y establecer alianzas estratégicas que nos permitan llegar a nuevos mercados y audiencias.</p>
+      </div>
+    </a>
+  </li>
+  <li>
+    <a href="" class="card">
+      <img src="https://i.ibb.co/dj7D2GL/ideales.png" class="card__image" alt="" />
+      <div class="card__overlay">
+        <div class="card__header">
+          <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+          <div class="card__header-text">
+            <h3 class="card__title">Valores</h3>
+          </div>
+        </div>
+        <p class="card__description">
+          Respeto
+          <br>
+          Responsabilidad
+          <br>
+          Pasión
+          <br>
+          Integridad
+          <br>
+          Calidad
+          <br>
+          Honestidad
+          <br>
+          Competitividad
+          <br>
+          Paciencia
+
+        </p>
+      </div>
+    </a>
+  </li>
+  
+</ul>
+
+<footer>
+    <div class="container" id="contacto">
+      <div class="content_footer">
+        <div class="profil">
+          <div class="logo_area">
+            <img src="https://i.ibb.co/m0K6W8d/zyro-image-18-Photoroom-png-Photoroom.png" alt="" width="40%" height="30%">
+         
+          </div>
+          <div class="desc_area">
+            <p> Ofrecemos productos con una amplia variedad de diseños para complacer el gusto personal e individual, siguiendo tendencias actuales y sugerentes del continuo cambio del diseño gráfico.</p>
+          </div>
+          <div class="social_media">
+            <a href="https://api.whatsapp.com/send?phone=18299620471&text=Hola%2C%20me%20interesa%20mas%20informaci%C3%B3n%20de%20tus%20productos.%20%20%20" class="whatsaap" ><i class='bx bxl-whatsapp-square'></i></a>
+            <a href="https://www.instagram.com/lasergraf.rd/" class="insta"><i class='bx bxl-instagram-alt'></i></a>
+            <a href="https://www.facebook.com/yudirodriguez11"><i class='bx bxl-facebook-square'></i></a>
+           
+          </div>
+        </div>
+        <div class="service_area">
+          <ul class="service_header">
+          <li class="service_name">Servicios</li>
+            <li><a href="#">Placas en acrílico.</a></li>
+            <li><a href="#about">Impresiones</a></li>
+            <li><a href="#">Letreros en neon</a></li>
+            <li><a href="#">Grabado láser</a></li>
+          </ul>
+          <ul class="service_header">
+            <li class="service_name">Menu</li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#nosotros">Nosotros</a></li>
+            <li><a href="http://localhost/hola/placas.php">Productos</a></li>
+            <li><a href="http://localhost/iNKA/registro.php">Registrate</a></li>
+          </ul>
+          <ul class="service_header">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.0312335506555!2d-70.70042432652866!3d19.45422013999344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eb1cf5f20cfd5fd%3A0xf4efa55d0d5dcd3a!2sLasergraf!5e0!3m2!1ses-419!2sdo!4v1708644762805!5m2!1ses-419!2sdo" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </ul>
+        </div>
+      </div>
+      <hr>
+      <div class="footer_buttom">
+        <div class="copy_right">
+        <i class='bx bxs-copyright'></i>
+        <span>2024 Laser graf.</span>
+        </div>
+        <div class="tou">
+          <a href="#">Politica de Privacidad</a>
+          <a href="#">Cookie</a>
+        </div>
+      </div>
+    </div>
+
+</footer>
+</body>
+</html>
